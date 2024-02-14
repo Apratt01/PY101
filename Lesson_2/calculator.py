79f3176f2e94fb391fbc12ddcd6caa24607bb06e
Aileen Pratt <aileen2868@gmail.com>
2024-02-14 03:58:16 +0000

    Added functionality to ask a user to go again.

--- Lesson_2/calculator.py
+++ Lesson_2/calculator.py
@@ -9,38 +9,63 @@ def invalid_number(number_str):
 
     return False
 
-prompt("Welcome to Calculator!")
-
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def number_one():
+    prompt("What's the first number?")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
-
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
+    return number1
+        
+def number_two():
+    prompt("What's the second number?")
     number2 = input()
 
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) "
-       "Multiply 4) Divide")
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
+    return number2
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+def operation_choice():
+    prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) "
+       "Multiply 4) Divide")
     operation = input()
 
-match operation:
-    case "1":
-        output = int(number1) + int(number2)
-    case "2":
-        output = int(number1) - int(number2)
-    case "3":
-        output = int(number1) * int(number2)
-    case "4":
-        output = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt("You must choose 1, 2, 3, or 4")
+        operation = input()
+    return operation
 
-prompt(f"The result is {output}")
+def math(number1, number2, choice):
+    output = 0
+    match choice:
+        case "1":
+            output = int(number1) + int(number2)
+        case "2":
+            output = int(number1) - int(number2)
+        case "3":
+            output = int(number1) * int(number2)
+        case "4":
+            output = int(number1) / int(number2)
+    prompt(f"The result is {output}")
+    
+def repeat():
+    prompt("Would you like to do another calculation? Yes to go again!")
+    answer = input()
+    
+    if answer.lower() == 'yes':
+        print("Great, let's go again!")
+        main()
+    else: print("Thank you for playing!")
+    
+
+def main():
+    prompt("Welcome to Calculator!")
+    number1 = number_one()
+    number2 = number_two()
+    choice = operation_choice()
+    math(number1, number2, choice)
+    repeat()
+    
+main()
