c650b57fa5291814023ef66a0d9c3c45282e2273
Aileen Pratt <aileen2868@gmail.com>
2024-02-15 01:55:07 +0000

    Finished adding a Spanish option

--- Lesson_2/calculator.py
+++ Lesson_2/calculator.py
@@ -2,9 +2,28 @@ import json
 
 with open('calculator_messages.json', 'r') as file:
     MESSAGES = json.load(file)
-
+    
 def prompt(message):
     print(f"==> {message}")
+
+def ask_language():
+    choices = ['en', 'sp', 'english', 'spanish', 'español']
+    prompt(MESSAGES["language_en"])
+    prompt(MESSAGES["language_sp"])
+    language = input()
+    
+    while language.lower() not in choices:
+        prompt(MESSAGES["invalid_language_en"])
+        prompt(MESSAGES["invalid_language_sp"])
+        language = input()
+    language_lower = language.lower()
+    return choose_language(language_lower)
+    
+def choose_language(language):
+    if language == 'en' or language == 'english':
+        return MESSAGES['en']
+    else:
+        return MESSAGES['sp']
 
 def invalid_number(number_str):
     try:
@@ -15,30 +34,30 @@ def invalid_number(number_str):
     return False
 
 def number_one():
-    prompt(MESSAGES["first_number"])
+    prompt(language_message["first_number"])
     number1 = input()
 
     while invalid_number(number1):
-        prompt(MESSAGES["invalid_number"])
+        prompt(language_message["invalid_number"])
         number1 = input()
     return number1
         
 def number_two():
-    prompt(MESSAGES["second_number"])
+    prompt(language_message["second_number"])
     number2 = input()
 
     while invalid_number(number2):
-        prompt(MESSAGES["invalid_number"])
+        prompt(language_message["invalid_number"])
         number2 = input()
     return number2
 
 def operation_choice():
-    operation_message = '\n'.join(MESSAGES['operation_message'])
+    operation_message = '\n'.join(language_message['operation_message'])
     prompt(operation_message)
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt(MESSAGES["invalid_choice"])
+        prompt(language_message["invalid_choice"])
         operation = input()
     return operation
 
@@ -53,16 +72,16 @@ def math(number1, number2, choice):
             output = int(number1) * int(number2)
         case "4":
             output = int(number1) / int(number2)
-    prompt(MESSAGES["result"].format(variable=output))
+    prompt(language_message["result"].format(variable=output))
     
 def repeat():
-    prompt(MESSAGES["repeat"])
+    prompt(language_message["repeat"])
     answer = input()
     
     if answer.lower() == 'yes':
-        print(MESSAGES["yes"])
+        print(language_message["yes"])
         main()
-    else: print(MESSAGES["no"])
+    else: print(language_message["no"])
     
 
 def main():
@@ -72,6 +91,8 @@ def main():
     math(number1, number2, choice)
     repeat()
 
-prompt(MESSAGES["welcome"])
+prompt(MESSAGES["en"]["welcome"])
+prompt(MESSAGES["sp"]["welcome"])
+language_message = ask_language()
 
 main()
