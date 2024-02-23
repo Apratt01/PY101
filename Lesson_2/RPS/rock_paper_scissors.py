944a143029e15d5709bb254bd807ef52625c967d
Aileen Pratt <aileen2868@gmail.com>
2024-02-23 22:46:04 +0000

    Added score card display, and loop to play again

--- Lesson_2/RPS/rock_paper_scissors.py
+++ Lesson_2/RPS/rock_paper_scissors.py
@@ -6,7 +6,11 @@ import os
 with open('rps_messages.json', 'r') as file:
     MESSAGES = json.load(file) # import JSON file
 
+
+# Declare Constants
+
 WINNING_SCORE = 5
+YES_NO_ANSWERS = ('y', 'yes', 'n', 'no')
 GAME_CHOICES = {
     'wins': {
         'scissors': ['paper', 'lizard'],
@@ -24,7 +28,9 @@ GAME_CHOICES = {
         }
     }
 
-def prompt(message):    
+# Start Functions
+
+def prompt(message):
     print(f"==> {message}")
 
 def clear_screen():
@@ -34,29 +40,31 @@ def clear_screen():
         os.system('cls')  # Clear screen on Windows
     else:
         os.system('clear')  # Clear screen on Unix/Linux/Mac
-        
+
 def get_rules_choice():
     prompt(MESSAGES["rules_read"])
-    answer = input()
-    
-    while answer.lower() not in ('y', 'yes', 'n', 'no'):
-        prompt(MESSAGES["yes_or_no"])
-        answer = input()
-    
-    answer = answer.lower()
-    if answer in ('y', 'yes'):
-        display_rules(answer)
+    rules_choice = input()
 
-def display_rules(answer):
+    while rules_choice.lower() not in YES_NO_ANSWERS:
+        prompt(MESSAGES["yes_or_no"])
+        rules_choice = input()
+
+    if rules_choice.lower() in ('y', 'yes'):
+        display_rules()
+    else:
+        clear_screen()
+
+def display_rules():
     rules = '\n'.join(MESSAGES['rules'])
     print(rules.format(winning_number=WINNING_SCORE))
-        
+
 def get_player_choice():
 
     while True:
         combined_list = []
         for initial, word in GAME_CHOICES['choices'].items():
-            prompt(MESSAGES['choose'].format(key=initial, value=word))
+            prompt(MESSAGES['choose'].format(key=initial,
+                value=word.capitalize()))
             combined_list.append(initial)
             combined_list.append(word)
         choice = input()
@@ -70,39 +78,49 @@ def get_player_choice():
 
 def get_computer_choice():
     list_of_words = []
-    
+
     for _, word in GAME_CHOICES['choices'].items():
         list_of_words.append(word)
-        
+
     computer = random.choice(list_of_words)
     return computer
-    
+
 def select_winner(player, computer):
-    
+    winner = ''
+
     if player in GAME_CHOICES['wins'][computer]:
-        return 'computer_wins'
+        winner = 'computer_wins'
     elif computer in GAME_CHOICES['wins'][player]:
-        return 'player_wins'
+        winner = 'player_wins'
     else:
-        return 'ties'
-        
+        winner = 'ties'
+
+    return winner
+
 def display_winner(player, computer, winner):
-    
-    prompt(MESSAGES["choice_recap"].format(player_choice=player,
-        computer_choice=computer))
-        
+
+    prompt(MESSAGES["choice_recap"].format(player_choice=player.capitalize(),
+        computer_choice=computer.capitalize()))
+
     if winner == 'player_wins':
         prompt(MESSAGES["player_win"])
     elif winner == 'computer_wins':
         prompt(MESSAGES["computer_win"])
     else:
         prompt(MESSAGES["tie"])
-        
+
 def update_score_tracker(winner, score_tracker):
     if winner in ('player_wins', 'computer_wins'):
         score_tracker[winner] += 1
     return score_tracker
-    
+
+def display_score(scores):
+    score_recap = '\n'.join(MESSAGES["score_recap"])
+    player_count = scores['player_wins']
+    computer_count = scores['computer_wins']
+    print(score_recap.format(player_wins=player_count,
+        computer_wins=computer_count))
+
 def display_grand_winner(score_tracker):
     for winner, score in score_tracker.items():
         if score == WINNING_SCORE:
@@ -111,30 +129,43 @@ def display_grand_winner(score_tracker):
                 prompt(MESSAGES["grand_winner"].format(winner="Player"))
             else:
                 prompt(MESSAGES["grand_winner"].format(winner="Computer"))
-    
+
 def start_game_play():
+    get_rules_choice()
+
+    score_tracker = {'player_wins': 0, 'computer_wins':0}
+
     while True:
-        score_tracker = {'player_wins': 0, 'computer_wins':0}
         list_of_scores = list(score_tracker.values())
-        
+
         while WINNING_SCORE not in list_of_scores:
-            
+
             player= get_player_choice()
             computer = get_computer_choice()
+            clear_screen()
             winner = select_winner(player, computer)
             display_winner(player, computer, winner)
             score_tracker = update_score_tracker(winner, score_tracker)
             list_of_scores = list(score_tracker.values())
-            print(score_tracker)
+            display_score(score_tracker)
         display_grand_winner(score_tracker)
         break
 
+clear_screen()
+prompt(MESSAGES['welcome'])
 
-def main():
-    clear_screen()
-    prompt(MESSAGES['welcome'])
-    clear_screen()
-    get_rules_choice()
+while True:
     start_game_play()
-    
-main()
+    prompt(MESSAGES["play_again"])
+    answer = input()
+
+    while answer.lower() not in YES_NO_ANSWERS:
+        prompt(MESSAGES["yes_or_no"])
+        answer = input()
+
+    if answer.lower() in ('n', 'no'):
+        break
+    clear_screen()
+
+clear_screen()
+prompt(MESSAGES['goodbye'])
